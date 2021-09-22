7e0c3d557371912d52fcf02b1af11b0177d2da29
danushan99 <danuthasan08@gmail.com>
2021-09-22 18:52:00 +0530

    3 rd

--- lib/mine.dart
+++ lib/mine.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:motion_toast/motion_toast.dart';
+import 'package:motion_toast/resources/arrays.dart';
 
+Widget toast = Container(
+  padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+  decoration: BoxDecoration(
+    borderRadius: BorderRadius.circular(25.0),
+    color: Colors.greenAccent,
+  ),
+  child: Row(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      Icon(Icons.check),
+      SizedBox(
+        width: 12.0,
+      ),
+      Text("Deleted"),
+    ],
+  ),
+);
